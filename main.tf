f1c074779c44585ddce50d88f85543336e1e0562
oabaiadze <76046294+oabaiadze@users.noreply.github.com>
2021-01-22 01:54:45 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "scalr" {
   ami                    = "ami-2757f631"
   instance_type          = "t2.micro"
-  subnet_id              = "subnet-0ebb1058ad727cfdb"
-  vpc_security_group_ids = ["sg-0880cfdc546b123ba"]
+  subnet_id              = "subnet-d5206699"
+  vpc_security_group_ids = ["sg-0c5100e692e446792"]
   key_name               = "terraform"
 }
